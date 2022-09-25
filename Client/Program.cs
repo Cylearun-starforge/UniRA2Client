29bf1e16257602a075d17978c23ecbf45cca3bc9
kaixa <690750353@qq.com>
2022-09-26 00:36:01 +0800

    style(core): format C# codes and edit .editorconfig

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,15 +2,15 @@ namespace UniRA2.Client;
 
 internal static class Program
 {
-	/// <summary>
-	///  The main entry point for the application.
-	/// </summary>
-	[STAThread]
-	static void Main(string[] args)
-	{
-		// To customize application configuration such as set high DPI settings or default font,
-		// see https://aka.ms/applicationconfiguration.
-		ApplicationConfiguration.Initialize();
-		Application.Run(new Form1());
-	}
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
 }
