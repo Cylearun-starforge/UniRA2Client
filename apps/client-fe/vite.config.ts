ea3b78577d8948d576fd32dedb49490feaa023c8
kaixa <690750353@qq.com>
2023-08-18 00:42:24 +0800

    feat: new tsconfig package

--- apps/client-fe/vite.config.ts
+++ apps/client-fe/vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
@@ -35,4 +35,4 @@ export default defineConfig(async () => ({
   define: {
     __DEV__: Boolean(process.env.TAURI_DEBUG),
   },
-}));
+});
