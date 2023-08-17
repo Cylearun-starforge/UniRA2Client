4c428df73a0b5ba55dde0f8d5e107d6c641716c4
kaixa <690750353@qq.com>
2023-08-18 00:47:22 +0800

    chore: generate .d.ts for ui-components

--- libs/ui-components/vite.config.ts
+++ libs/ui-components/vite.config.ts
@@ -3,10 +3,11 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), dts({rollupTypes: true})],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
